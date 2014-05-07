023cd620981b41f01b059a9340bf7a2b6a6f913f
Sandro Bessa <ext-sandro.lima@nokia.com.br>
2014-05-07 15:32:00 -0400

    Integração da base com o parser

--- TesouroNacional/src/com/csd/tesouronacional/database/DataLayer.java
+++ TesouroNacional/src/com/csd/tesouronacional/database/DataLayer.java
@@ -3,69 +3,91 @@ package com.csd.tesouronacional.database;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.csd.tesouronacional.database.TituloDBHelper.TituloEntry;
-import com.csd.tesouronacional.model.Titulo;
-
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.csd.tesouronacional.database.TituloDBHelper.TituloEntry;
+import com.csd.tesouronacional.model.Titulo;
+import com.csd.tesouronacional.parser.TesouroParser;
 
 public class DataLayer {
 	public static final DataLayer instance = new DataLayer();
-	
+
 	private TituloDBHelper helper;
 	
 	private DataLayer(){}
 	
 	public void init (Context context){
 		helper = new TituloDBHelper (context);
-	}
-	
-	public long insertTitulo(Titulo titulo){
-		SQLiteDatabase db = helper.getWritableDatabase();
+		List<Titulo> list = null;
+
+		try {
+			list = TesouroParser.parseTesouroURL();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		
+		for (Titulo titulo : list) {
+			System.out.println(titulo.getLetra());
+			insertTitulo(titulo);
+		}
+
+		readTitulos();
+	}
+
+	public long insertTitulo(Titulo titulo) {
+		SQLiteDatabase db = helper.getWritableDatabase();
+
 		ContentValues values = new ContentValues();
-        values.put(TituloEntry.COLUMN_NAME_ID, titulo.getId() );
-        values.put(TituloEntry.COLUMN_NAME_LETRA, titulo.getLetra() );
-        values.put(TituloEntry.COLUMN_NAME_NUMERO, titulo.getNumero() );
-        values.put(TituloEntry.COLUMN_NAME_VENCIMENTO, titulo.getFormattedVencimento());
-        values.put(TituloEntry.COLUMN_NAME_TX_COMPRA, titulo.getTaxaCompra());
-        values.put(TituloEntry.COLUMN_NAME_PRECO_COMPRA, titulo.getPrecoCompra());
-        
-        return db.insert(TituloEntry.TABLE_NAME, "", values);
-    }
-	
-	public List<Titulo> readTitulos (){
+		values.put(TituloEntry.COLUMN_NAME_ID, titulo.getId());
+		values.put(TituloEntry.COLUMN_NAME_LETRA, titulo.getLetra());
+		values.put(TituloEntry.COLUMN_NAME_NUMERO, titulo.getNumero());
+		values.put(TituloEntry.COLUMN_NAME_VENCIMENTO,
+				titulo.getFormattedVencimento());
+		values.put(TituloEntry.COLUMN_NAME_TX_COMPRA, titulo.getTaxaCompra());
+		values.put(TituloEntry.COLUMN_NAME_PRECO_COMPRA,
+				titulo.getPrecoCompra());
+
+		return db.insert(TituloEntry.TABLE_NAME, "", values);
+	}
+
+	public List<Titulo> readTitulos() {
 		ArrayList<Titulo> list = new ArrayList<Titulo>();
 		SQLiteDatabase db = helper.getReadableDatabase();
-		
-		String [] columns = {
-			TituloEntry._ID,TituloEntry.COLUMN_NAME_ID, TituloEntry.COLUMN_NAME_LETRA,TituloEntry.COLUMN_NAME_NUMERO,
-			TituloEntry.COLUMN_NAME_VENCIMENTO,TituloEntry.COLUMN_NAME_TX_COMPRA,TituloEntry.COLUMN_NAME_TX_COMPRA
-				
+
+		String[] columns = { TituloEntry._ID, TituloEntry.COLUMN_NAME_ID,
+				TituloEntry.COLUMN_NAME_LETRA, TituloEntry.COLUMN_NAME_NUMERO,
+				TituloEntry.COLUMN_NAME_VENCIMENTO,
+				TituloEntry.COLUMN_NAME_TX_COMPRA,
+				TituloEntry.COLUMN_NAME_TX_COMPRA
+
 		};
-	
-		String groupBy = TituloEntry.COLUMN_NAME_ID;	
+
+		String groupBy = TituloEntry.COLUMN_NAME_ID;
 		String filter = TituloEntry.COLUMN_NAME_ID + ">" + Integer.toString(0);
-		String orderBy =  TituloEntry.COLUMN_NAME_TX_COMPRA + " DESC";
-		Cursor c = db.query(false, TituloEntry.TABLE_NAME, columns,
-		        null, null, groupBy, filter, orderBy, null);
-		
-		while (c.moveToNext()){		
+		String orderBy = TituloEntry.COLUMN_NAME_TX_COMPRA + " DESC";
+		Cursor c = db.query(false, TituloEntry.TABLE_NAME, columns, null, null,
+				groupBy, filter, orderBy, null);
+
+		while (c.moveToNext()) {
 			Titulo titulo = new Titulo();
-			
+
 			titulo.setId(c.getLong(1));
 			titulo.setLetra(c.getString(2));
 			titulo.setNumero(c.getString(3));
 			titulo.setVencimento(c.getString(4));
 			titulo.setTaxaCompra(c.getDouble(5));
 			titulo.setPrecoCompra(c.getDouble(6));
-			
-			System.out.println("Título::"+titulo.getLetra());
-			System.out.println("Vencimento::"+titulo.getVencimento());
-			System.out.println("Taxa::"+titulo.getTaxaCompra());
-		
+
+			System.out.println("Título::" + titulo.getLetra());
+			System.out.println("Vencimento::" + titulo.getVencimento());
+			System.out.println("Taxa::" + titulo.getTaxaCompra());
+
 			list.add(titulo);
 		}
 
--- TesouroNacional/src/com/csd/tesouronacional/parser/TesouroParser.java
+++ TesouroNacional/src/com/csd/tesouronacional/parser/TesouroParser.java
@@ -9,79 +9,95 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.util.Log;
+
 import com.csd.tesouronacional.model.Titulo;
 
 public class TesouroParser {
 
-    private static final String TESOURO_URL = "http://www3.tesouro.gov.br/tesouro_direto/consulta_titulos_novosite/consultatitulos.asp";
+	private static final String TESOURO_URL = "http://www3.tesouro.gov.br/tesouro_direto/consulta_titulos_novosite/consultatitulos.asp";
 
-    public static List<Titulo> parseTesouroURL() throws IOException, ParseException {
-        String content = getURLContent(TESOURO_URL);
-        return parseContent(content);
-    }
+	public static List<Titulo> parseTesouroURL() throws IOException,
+			ParseException {
+		String content = getURLContent(TESOURO_URL);
+		return parseContent(content);
+	}
 
-    private static List<Titulo> parseContent(String content) throws ParseException {
-        List<Titulo> titulos = new ArrayList<Titulo>();
-        int startTitulo = 0;
-        int endTitulo;
-        for (int i = 0; i < 24; i++) {
-            try {
-                Titulo t = new Titulo();
+	private static List<Titulo> parseContent(String content)
+			throws ParseException {
+		List<Titulo> titulos = new ArrayList<Titulo>();
+		int startTitulo = 0;
+		int endTitulo;
+		for (int i = 0; i < 24; i++) {
+			try {
+				Titulo t = new Titulo();
+				String tituloKey = "<TD  class=\"listing0\" align=left>";
+				startTitulo = content.indexOf(tituloKey, startTitulo);
+				endTitulo = content.indexOf("</TD>", startTitulo);
+				t.setLetra(content.substring(startTitulo + tituloKey.length(),
+						endTitulo));
 
-                String tituloKey = "<TD  class=\"listing0\" align=left>";
-                startTitulo = content.indexOf(tituloKey, startTitulo);
-                endTitulo = content.indexOf("</TD>", startTitulo);
-                t.setLetra(content.substring(startTitulo + tituloKey.length(), endTitulo));
+				String vencimentoKey = "<TD";
+				int startVencimento = content.indexOf(vencimentoKey, endTitulo);
+				int endVencimento = content.indexOf("</TD>", startVencimento);
+				startVencimento = content.indexOf('>', startVencimento) + 1;
+				SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+				t.setVencimento(sdf.parse(content.substring(startVencimento,
+						endVencimento)));
 
-                String vencimentoKey = "<TD";
-                int startVencimento = content.indexOf(vencimentoKey, endTitulo);
-                int endVencimento = content.indexOf("</TD>", startVencimento);
-                startVencimento = content.indexOf('>', startVencimento) + 1;
-                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-                t.setVencimento(sdf.parse(content.substring(startVencimento, endVencimento)));
+				String taxaCompraKey = "<TD";
+				int startTaxaCompra = content.indexOf(taxaCompraKey,
+						endVencimento);
+				int endTaxaCompra = content.indexOf("</TD>", startTaxaCompra);
+				startTaxaCompra = content.indexOf('>', startTaxaCompra) + 1;
+				String strTaxaCompra = content.substring(startTaxaCompra,
+						endTaxaCompra - 1);
+				strTaxaCompra = strTaxaCompra.replace(',', '.');
+				t.setTaxaCompra(Double.parseDouble(strTaxaCompra));
 
-                String taxaCompraKey = "<TD";
-                int startTaxaCompra = content.indexOf(taxaCompraKey, endVencimento);
-                int endTaxaCompra = content.indexOf("</TD>", startTaxaCompra);
-                startTaxaCompra = content.indexOf('>', startTaxaCompra) + 1;
-                String strTaxaCompra = content.substring(startTaxaCompra, endTaxaCompra - 1);
-                strTaxaCompra = strTaxaCompra.replace(',', '.');
-                t.setTaxaCompra(Double.parseDouble(strTaxaCompra));
+				String taxaVendaKey = "<TD";
+				int startTaxaVenda = content.indexOf(taxaVendaKey,
+						endTaxaCompra);
+				int endTaxaVenda = content.indexOf("</TD>", startTaxaVenda);
+				startTaxaVenda = content.indexOf('>', startTaxaVenda) + 1;
 
-                String taxaVendaKey = "<TD";
-                int startTaxaVenda = content.indexOf(taxaVendaKey, endTaxaCompra);
-                int endTaxaVenda = content.indexOf("</TD>", startTaxaVenda);
-                startTaxaVenda = content.indexOf('>', startTaxaVenda) + 1;
+				String valorCompraKey = "<TD";
+				int startValorCompra = content.indexOf(valorCompraKey,
+						endTaxaVenda);
+				int endValorCompra = content.indexOf("</TD>", startValorCompra);
+				startValorCompra = content.indexOf('>', startValorCompra) + 1;
+				String strValorCompra = content.substring(startValorCompra + 3,
+						endValorCompra);
+				strValorCompra = strValorCompra.replace(".", "").replace(',',
+						'.');
+				t.setPrecoCompra(Double.parseDouble(strValorCompra));
 
-                String valorCompraKey = "<TD";
-                int startValorCompra = content.indexOf(valorCompraKey, endTaxaVenda);
-                int endValorCompra = content.indexOf("</TD>", startValorCompra);
-                startValorCompra = content.indexOf('>', startValorCompra) + 1;
-                String strValorCompra = content.substring(startValorCompra + 3, endValorCompra);
-                strValorCompra = strValorCompra.replace(".", "").replace(',', '.');
-                t.setPrecoCompra(Double.parseDouble(strValorCompra));
+				Log.e("Conteudo::", t.toString());
 
-                titulos.add(t);
-            } catch (NumberFormatException e) {
-                startTitulo++;
-                continue;
-            }
-            startTitulo++;
-        }
-        return titulos;
-    }
+				titulos.add(t);
+			} catch (NumberFormatException e) {
+				startTitulo++;
+				continue;
+			}
+			startTitulo++;
+		}
+		return titulos;
+	}
 
-    private static String getURLContent(String url) throws IOException {
-        URL yahoo = new URL(url);
-        BufferedReader in = new BufferedReader(new InputStreamReader(yahoo.openStream()));
+	private static String getURLContent(String url) throws IOException {
+		URL yahoo = new URL(url);
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				yahoo.openStream()));
 
-        StringBuilder sb = new StringBuilder();
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            sb.append(inputLine);
-        in.close();
+		StringBuilder sb = new StringBuilder();
+		String inputLine;
+		while ((inputLine = in.readLine()) != null)
+			sb.append(inputLine);
+		in.close();
 
-        return sb.toString();
-    }
+		System.out.println("Arquivo::" + sb.length());
+
+		return sb.toString();
+	}
 
 }
